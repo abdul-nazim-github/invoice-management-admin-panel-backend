e5b317d0cb9c7e42d30c043d4d14dec958e90408
abdul.nazim <abdul.nazim@codeclouds.com>
2025-09-14 17:12:30 +0530

    Fix - update customer model

--- app/utils/pagination.py
+++ app/utils/pagination.py
@@ -6,9 +6,9 @@ from flask import request
 def get_pagination():
     try:
         page = int(request.args.get("page", 1))
-        limit = int(request.args.get("limit", 20))
+        limit = int(request.args.get("limit", 10))
     except ValueError:
-        page, limit = 1, 20
+        page, limit = 1, 10
     page = max(page, 1)
     limit = max(min(limit, 100), 1)
     offset = (page - 1) * limit
