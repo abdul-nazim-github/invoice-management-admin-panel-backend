2fe29cb595f1ce7e51e93fc1a8169c4eaad37e17
sknazim1818 <sknazim1818@gmail.com>
2025-09-24 16:55:10 +0000

    Fix - routes

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from app.routes.users import users_blueprint
+from app.api.auth.routes import auth_bp
+from app.api.users.routes import users_bp
 from app.routes.customers import customers_blueprint
 from app.routes.products import products_blueprint
 from app.routes.invoices import invoices_blueprint
@@ -8,7 +9,8 @@ from app.routes.payments import payments_blueprint
 def create_app():
     app = Flask(__name__)
 
-    app.register_blueprint(users_blueprint)
+    app.register_blueprint(auth_bp, url_prefix='/api/auth')
+    app.register_blueprint(users_bp, url_prefix='/api/users')
     app.register_blueprint(customers_blueprint)
     app.register_blueprint(products_blueprint)
     app.register_blueprint(invoices_blueprint)
