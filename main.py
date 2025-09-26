c3763ee16bc1fa980ff75a5a249dfaf6431a0b96
sknazim1818 <sknazim1818@gmail.com>
2025-09-26 15:36:19 +0000

    Fix - changez

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from app import create_app
 from app.database.schemas.schema import create_schemas
+from seed import seed_initial_admin
 
 # Create the database schema before running the app
 create_schemas()
+
+# Seed the database with the initial admin user
+seed_initial_admin()
 
 app = create_app()
 
