2cb088302c05d0b846b737a7447b2862611c17e7
abdul.nazim <abdul.nazim@codeclouds.com>
2025-09-26 20:57:06 +0530

    Fix - db config

--- app/database/base.py
+++ app/database/base.py
@@ -5,7 +5,7 @@ def get_db_connection():
     return pymysql.connect(
         host=os.environ.get('DB_HOST', 'localhost'),
         user=os.environ.get('DB_USER', 'root'),
-        password=os.environ.get('DB_PASSWORD', 'password'),
-        database=os.environ.get('DB_NAME', 'invoicify'),
+        password=os.environ.get('DB_PASSWORD', 'root'),
+        database=os.environ.get('DB_NAME', 'vyaper_billing_db'),
         cursorclass=pymysql.cursors.DictCursor
     )
