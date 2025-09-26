55bdc7c6fb863125b219a243595ef182c7b94610
sknazim1818 <sknazim1818@gmail.com>
2025-09-26 14:56:07 +0000

    Add schema

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,25 @@
 from flask import Flask
 from .utils.cache import cache
+from app.api.auth.routes import auth_bp
+from app.api.users.routes import users_bp
+from .routes.customers import customers_blueprint
+from .routes.invoices import invoices_blueprint
+from .routes.payments import payments_blueprint
+from .routes.products import products_blueprint
+from .api.dashboard.routes import dashboard_bp
+from app.database.schema import create_schema
 
 def create_app():
     app = Flask(__name__)
 
     # Initialize the cache with the app
     cache.init_app(app, config={'CACHE_TYPE': 'SimpleCache'})
+    
+    create_schema()
 
     # Import and register your Blueprints here
-    from .routes.customers import customers_blueprint
-    from .routes.invoices import invoices_blueprint
-    from .routes.payments import payments_blueprint
-    from .routes.products import products_blueprint
-    from .api.dashboard.routes import dashboard_bp
-
+    app.register_blueprint(auth_bp, url_prefix='/api/auth')
+    app.register_blueprint(users_bp, url_prefix='/api/users')
     app.register_blueprint(customers_blueprint, url_prefix='/api')
     app.register_blueprint(invoices_blueprint, url_prefix='/api')
     app.register_blueprint(payments_blueprint, url_prefix='/api')
