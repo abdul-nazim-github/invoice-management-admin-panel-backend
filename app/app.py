31ea40026b87de7543b9fb658459676a74cd286a
sknazim1818 <sknazim1818@gmail.com>
2025-09-26 14:52:21 +0000

    Add schema

--- app/app.py
+++ app/app.py
@@ -5,9 +5,12 @@ from app.routes.customers import customers_blueprint
 from app.routes.products import products_blueprint
 from app.routes.invoices import invoices_blueprint
 from app.routes.payments import payments_blueprint
+from app.database.schema import create_schema
 
 def create_app():
     app = Flask(__name__)
+
+    create_schema()
 
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
     app.register_blueprint(users_bp, url_prefix='/api/users')
